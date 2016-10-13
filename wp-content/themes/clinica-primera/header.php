c398ebf2ff1e2e644dcc47d1232265d78477ef58
Leoni <leoni@agenciadelucca.com.br>
2016-10-13 13:54:43 +0000

    Setup finished

---
+++ wp-content/themes/clinica-primera/header.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Clínica_Primera
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<!-- Icons -->
+<link rel="icon" href="<?php bloginfo("stylesheet_directory"); ?>/assets/images/logo/logo-primera_16.png" size="16x16">
+<link rel="icon" href="<?php bloginfo("stylesheet_directory"); ?>/assets/images/logo/logo-primera_32.png" size="32x32">
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+<!-- Local Files -->
+<link href="<?php bloginfo("stylesheet_directory"); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php bloginfo("stylesheet_directory"); ?>/assets/css/ionicons.min.css" rel="stylesheet">
+<link href="<?php bloginfo("stylesheet_directory"); ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="<?php bloginfo("stylesheet_directory"); ?>/assets/css/jquery.bxslider.css" rel="stylesheet">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="site">
+	
